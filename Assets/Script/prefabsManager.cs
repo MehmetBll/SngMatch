571d984b315ffd97e70e07525e0545578a0b8dab
MehmetBll <sng0mehmet6@gmail.com>
2025-11-20 15:42:17 +0300

    position col

--- Assets/Script/prefabsManager.cs
+++ Assets/Script/prefabsManager.cs
@@ -1,18 +1,35 @@
 using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class prefabManager : MonoBehaviour
 {
     public GameObject[] prefabs;
-    void Awake()
-    {
-        prefabs = Resources.LoadAll<GameObject>("Prefabs");
-    }
+    //denemelik
+    public int spawnCount=10;
+    public float posX = 7f;
+    public float posY = 10f;
+    public float posZ = 12f;
+
     void Start()
     {
+        SpawnObjects();
+    }
+    void SpawnObjects()
+    {
+        
+            for (int i = 0; i < spawnCount; i++)
+        {
+            Vector3 randomPoz = new Vector3(
+                Random.Range(-posX,posX),
+                Random.Range(posY, posY),
+                Random.Range(-posZ, posZ)
+            );
         foreach(GameObject prefab in prefabs)
         {
-            Instantiate(prefab,Vector3.zero,quaternion.identity);
+            Instantiate(prefab,randomPoz,Quaternion.identity);
+        }
+    
         }
     }
 }
