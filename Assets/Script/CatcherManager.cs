dbedf5cacaf4c1ee04445c805e5cf3190c9a11dd
MehmetBll <sng0mehmet6@gmail.com>
2025-12-25 15:55:03 +0300

    project 2.2
    throwup,particle

--- Assets/Script/CatcherManager.cs
+++ Assets/Script/CatcherManager.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine.AI;
 using System.Collections;
+using System.Xml.Serialization;
+using UnityEngine.SocialPlatforms.GameCenter;
+using UnityEngine.Analytics;
 
 public class CatcherManager : MonoBehaviour
 {
@@ -17,6 +20,7 @@ public class CatcherManager : MonoBehaviour
     public float processDelay = 0.05f;
     public float throwUpForce = 5f;
     public Transform returnPoint; 
+    public Material pieceMaterial;  
     public GameObject[] cWalls;
     private static CatcherManager CatcherL;
     private static CatcherManager CatcherR;
@@ -103,6 +107,10 @@ public class CatcherManager : MonoBehaviour
 
         if (id1 == id2)
         {
+            
+            BreakPieces(obj1);
+            BreakPieces(obj2);
+
             Destroy(obj1.gameObject);
             Destroy(obj2.gameObject);
         }
@@ -139,15 +147,24 @@ public class CatcherManager : MonoBehaviour
         }
         rb.isKinematic = false;
         rb.useGravity = true;
+
         rb.velocity = Vector3.zero;
         rb.angularVelocity = Vector3.zero;
 
-        Vector3 dir = new Vector3(0.5f, 2f, 3f).normalized;
-        rb.AddForce(dir * throwUpForce, ForceMode.Impulse);
-         Debug.Log("Throwup started");
-        yield return new WaitForSeconds(1.0f);
+       float sideOffset = Random.Range(-1f , 1f);
+
+       Vector3 throwDir =
+       Vector3.up * Random.Range(1.5f, 2.5f) +
+       Vector3.forward * Random.Range(2f, 3f) +
+         Vector3.right * sideOffset;
+
+         throwDir.Normalize();
+        rb.AddForce(throwDir * throwUpForce, ForceMode.Impulse);
+        rb.AddTorque(Random.insideUnitSphere * 5f , ForceMode.Impulse);
+
+        yield return new WaitForSeconds(2.0f);
         SetCWallsActive(true);
-        Debug.Log("Throwup ended and walls reactivated");
+
     }
 
     [System.Obsolete]
@@ -176,5 +193,39 @@ public class CatcherManager : MonoBehaviour
        Debug.Log("Throwup called");
        
     }
+    void BreakPieces(objectId oid)
+    {
+         Renderer rend = oid.GetComponentInChildren<Renderer>();
+           if (rend == null) 
+                return;
+
+                Vector3 center = rend.bounds.center;
+
+        for(int i =0; i<oid.pieceCount; i++)
+        {
+                Vector3 spawnPos = center + Random.insideUnitSphere * 0.5f;
+
+                GameObject piece = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                piece.transform.position = spawnPos;
+                piece.transform.localScale = Vector3.one * 0.2f;
+
+                Rigidbody rb = piece.AddComponent<Rigidbody>();
+                rb.mass = 0.1f;
+                rb.collisionDetectionMode = CollisionDetectionMode.Continuous;
+
+                Vector3 forceDir = (spawnPos - center).normalized;
+                rb.AddForce(forceDir * Random.Range(2f, 5f), ForceMode.Impulse);
+                rb.AddTorque(Random.insideUnitSphere * 5f);
+
+                Renderer r =piece.GetComponent<Renderer>();
+
+                Material matInstance = new Material(pieceMaterial);
+                matInstance.color = oid.effectColor;
+                r.material = matInstance;
+
+                Destroy(piece, 2.0f);
+        }
+    }
+    
 }
     
--- Assets/Script/objectId.cs
+++ Assets/Script/objectId.cs
@@ -5,13 +5,11 @@ public class objectId : MonoBehaviour
 public int matchId = 0;
 public Vector3 originalPosition;
 public string prefabName;
+public int pieceCount = 12;
+public Color effectColor = Color.white;
     private void Awake()
     {
         originalPosition = transform.position;
-        prefabName = gameObject.name;
-    }
-    private void OValidate()
-    {
         prefabName = gameObject.name;
     }
     public bool IsMatch(objectId other, bool requireNonZero = true)
