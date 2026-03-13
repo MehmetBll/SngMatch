f796596780f6e158edb7369cae2f2a53d785e577
mehmetballi0638-debug <mehmetballi0638@gmail.com>
2026-03-13 20:18:51 +0300

    restart fixed

--- Assets/Script/CWalls.cs
+++ Assets/Script/CWalls.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class CWalls : MonoBehaviour
 {
 private Collider[] wallColliders;
+private Collider GetCollider;
 private void Start()
     {
         SetWallsActive(true);
